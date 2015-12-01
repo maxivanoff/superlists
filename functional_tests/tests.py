e120f17e048641d8e4de42c645214058fff9be8e
Maxim Ivanov <maxim.ivanov@marquette.edu>
2015-12-01 14:49:02 -0600

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -32,10 +32,11 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/list/.+')
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
@@ -61,7 +62,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis has his own unique URL
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, 'lists/.+')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # again, there is no trace of Edith's list
